dd98e115d0be8204ead6892c6c1401c1b1bb7917
Gelzieny R. Martins <gelzieny@gmail.com>
2024-11-27 20:06:37 -0300

    Finalizando projeto

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,22 @@
 import { Dialog } from './components/ui/dialog'
 
-// import { EmptyGoals } from './components/empty-goals'
-import { CreateGoal } from './components/create-goals'
 import { Summary } from './components/summary'
+import { getSummary } from './http/get-summary'
+import { useQuery } from '@tanstack/react-query'
+import { EmptyGoals } from './components/empty-goals'
+import { CreateGoal } from './components/create-goals'
 
 export function App() {
+  const { data } = useQuery({
+    queryKey: ['summary'],
+    queryFn: getSummary,
+    staleTime: 1000 * 60,
+  })
+
   return (
     <Dialog>
-      {/* <EmptyGoals /> */}
+      {data?.total && data.total > 0 ? <Summary /> : <EmptyGoals />}
       <CreateGoal />
-      <Summary />
     </Dialog>
   )
 }
